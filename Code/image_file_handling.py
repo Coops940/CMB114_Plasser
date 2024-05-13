c040b7514b0e3d7269248b57256b1ed43686be2f
Coops940 <etcoops@gmail.com>
2024-05-13 13:45:36 +0100

    All code inergrated

--- Code/image_file_handling.py
+++ Code/image_file_handling.py
@@ -1,16 +1,27 @@
-from PIL import Image #Image hangling library
-orb_type = "2s"
+from PIL import Image
+from datetime import date #Imports to create individul name for saved plots
+from datetime import datetime
 
 def open_orbital_image(orb_type):
     dir_name = "Rendered_orbitals_EXAMPLES\\"
     try:
-        filename = str(orb_type + "_rendered_orbital.png") #Creating file name
-        print(filename)
-        pic_file = Image.open(dir_name+filename)
-        return pic_file
+        filename = str(dir_name+orb_type + "_rendered_orbital.png") #Creating file name
+        pic_file = Image.open(filename)
+        #pic_file = pic_file.resize((300,300),Image.ANTIALIAS)
+        return pic_file, filename
     except:
         print("Error: No file found")
     return
 
-pic_file = open_orbital_image(orb_type)
-pic_file.show()
+def create_filename(orb_type, plot_type):
+    return (orb_type +"_"+ plot_type +"_"+ str(date.today()) + '_' + str(datetime.now().strftime("%H-%M-%S")))
+
+def save_plot(plot, orb_type, plot_type):
+    filename = create_filename(orb_type, plot_type)
+    plot.savefig(filename+".png", dpi = 65)
+    return filename+".png"
+
+def open_plot_image(filename):
+    pic_file = Image.open(filename)
+    #pic_file = pic_file.resize((300,300),Image.ANTIALIAS)
+    return pic_file
